c49eaa3c7b36733ed968c3facee5d807a7e855c4
ochotonida <37985539+ochotonida@users.noreply.github.com>
2021-04-25 14:45:16 +0200

    Fix slots appearing when using /reset + rework some stuff

--- src/main/java/curiousarmorstands/CurioInventoryCapability.java
+++ src/main/java/curiousarmorstands/CurioInventoryCapability.java
@@ -1,21 +1,32 @@
+/*
+ * See https://github.com/TheIllusiveC4/Curios/blob/1.16.x-forge/src/main/java/top/theillusivec4/curios/common/capability/CurioInventoryCapability.java
+ */
+
 package curiousarmorstands;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.INBT;
+import net.minecraft.nbt.ListNBT;
 import net.minecraft.util.Direction;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.Tuple;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.common.util.LazyOptional;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.CuriosCapability;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.ISlotType;
 import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 import top.theillusivec4.curios.api.type.inventory.IDynamicStackHandler;
+import top.theillusivec4.curios.api.type.util.ISlotHelper;
+import top.theillusivec4.curios.common.inventory.CurioStacksHandler;
 
 import javax.annotation.Nonnull;
 import java.util.*;
@@ -50,24 +61,24 @@ public class CurioInventoryCapability {
 
         @Override
         public int getSlots() {
-            int slots = 0;
+            int totalSlots = 0;
 
             for (ICurioStacksHandler stacks : curios.values()) {
-                slots += stacks.getSlots();
+                totalSlots += stacks.getSlots();
             }
-            return slots;
+            return totalSlots;
         }
 
         @Override
         public int getVisibleSlots() {
-            int slots = 0;
+            int totalSlots = 0;
 
             for (ICurioStacksHandler stacks : curios.values()) {
                 if (stacks.isVisible()) {
-                    slots += stacks.getSlots();
+                    totalSlots += stacks.getSlots();
                 }
             }
-            return slots;
+            return totalSlots;
         }
 
         @Override
@@ -109,7 +120,7 @@ public class CurioInventoryCapability {
         @Override
         public void shrinkSlotType(String identifier, int amount) {
             if (amount > 0) {
-                this.getStacksHandler(identifier).ifPresent(stackHandler -> {
+                getStacksHandler(identifier).ifPresent(stackHandler -> {
                     int toShrink = Math.min(stackHandler.getSlots() - 1, amount);
                     loseStacks(stackHandler.getStacks(), identifier, toShrink);
                     stackHandler.shrink(amount);
@@ -142,10 +153,13 @@ public class CurioInventoryCapability {
                 for (int i = stackHandler.getSlots() - amount; i < stackHandler.getSlots(); i++) {
                     ItemStack stack = stackHandler.getStackInSlot(i);
                     drops.add(stackHandler.getStackInSlot(i));
+                    SlotContext slotContext = new SlotContext(identifier, wearer, i);
 
                     if (!stack.isEmpty()) {
-                        wearer.getAttributeManager().removeModifiers(CuriosApi.getCuriosHelper().getAttributeModifiers(identifier, stack));
+                        UUID uuid = UUID.nameUUIDFromBytes((identifier + i).getBytes());
+                        wearer.getAttributeManager().removeModifiers(CuriosApi.getCuriosHelper().getAttributeModifiers(slotContext, uuid, stack));
                         final int index = i;
+                        // noinspection deprecation
                         CuriosApi.getCuriosHelper().getCurio(stack).ifPresent(curio -> curio.onUnequip(identifier, index, wearer));
                     }
                     stackHandler.setStackInSlot(i, ItemStack.EMPTY);
@@ -172,17 +186,19 @@ public class CurioInventoryCapability {
         }
 
         @Override
-        public void setEnchantmentBonuses(Tuple<Integer, Integer> tuple) { }
+        public void setEnchantmentBonuses(Tuple<Integer, Integer> tuple) {
+
+        }
     }
 
     public static class Provider implements ICapabilitySerializable<INBT> {
 
         final LazyOptional<ICuriosItemHandler> optional;
-        final ICuriosItemHandler handler;
+        final ICuriosItemHandler instance;
 
         Provider(LivingEntity entity) {
-            handler = new CurioInventoryWrapper(entity);
-            optional = LazyOptional.of(() -> handler);
+            instance = new CurioInventoryWrapper(entity);
+            optional = LazyOptional.of(() -> instance);
         }
 
         @Nonnull
@@ -193,12 +209,81 @@ public class CurioInventoryCapability {
 
         @Override
         public INBT serializeNBT() {
-            return CuriosCapability.INVENTORY.writeNBT(handler, null);
+            CompoundNBT result = new CompoundNBT();
+            ListNBT slotTypes = new ListNBT();
+            instance.getCurios().forEach((key, stacksHandler) -> {
+                CompoundNBT tag = new CompoundNBT();
+                tag.put("StacksHandler", stacksHandler.serializeNBT());
+                tag.putString("Identifier", key);
+                slotTypes.add(tag);
+            });
+            result.put("Curios", slotTypes);
+            return result;
         }
 
         @Override
         public void deserializeNBT(INBT nbt) {
-            CuriosCapability.INVENTORY.readNBT(handler, null, nbt);
+            ListNBT slotTypes = ((CompoundNBT) nbt).getList("Curios", Constants.NBT.TAG_COMPOUND);
+            ISlotHelper slotHelper = CuriosApi.getSlotHelper();
+
+            if (!slotTypes.isEmpty() && slotHelper != null) {
+                Map<String, ICurioStacksHandler> curios = new LinkedHashMap<>();
+                SortedMap<ISlotType, ICurioStacksHandler> sortedCurios = slotHelper.createSlots();
+
+                for (int slotTypeIndex = 0; slotTypeIndex < slotTypes.size(); slotTypeIndex++) {
+                    CompoundNBT curioTag = slotTypes.getCompound(slotTypeIndex);
+                    String identifier = curioTag.getString("Identifier");
+                    CurioStacksHandler prevStacksHandler = new CurioStacksHandler();
+                    prevStacksHandler.deserializeNBT(curioTag.getCompound("StacksHandler"));
+
+                    Optional<ISlotType> optionalSlotType = CuriosApi.getSlotHelper().getSlotType(identifier);
+                    optionalSlotType.ifPresent(slotType -> {
+                        CurioStacksHandler newStacksHandler = new CurioStacksHandler(slotType.getSize(), prevStacksHandler.getSizeShift(), slotType.isVisible(), slotType.hasCosmetic());
+                        
+                        int slotIndex = 0;
+                        while (slotIndex < newStacksHandler.getSlots() && slotIndex < prevStacksHandler.getSlots()) {
+                            ItemStack prevStack = prevStacksHandler.getStacks().getStackInSlot(slotIndex);
+                            if (!prevStack.isEmpty()) {
+                                newStacksHandler.getStacks().setStackInSlot(slotIndex, prevStack);
+                            }
+
+                            ItemStack prevCosmetic = prevStacksHandler.getCosmeticStacks().getStackInSlot(slotIndex);
+                            if (!prevCosmetic.isEmpty()) {
+                                newStacksHandler.getCosmeticStacks().setStackInSlot(slotIndex, prevStacksHandler.getCosmeticStacks().getStackInSlot(slotIndex));
+                            }
+                            
+                            slotIndex++;
+                        }
+
+                        while (slotIndex < prevStacksHandler.getSlots()) {
+                            instance.loseInvalidStack(prevStacksHandler.getStacks().getStackInSlot(slotIndex));
+                            instance.loseInvalidStack(prevStacksHandler.getCosmeticStacks().getStackInSlot(slotIndex));
+                            slotIndex++;
+                        }
+                        
+                        sortedCurios.put(slotType, newStacksHandler);
+                    });
+
+                    if (!optionalSlotType.isPresent()) {
+                        IDynamicStackHandler stackHandler = prevStacksHandler.getStacks();
+                        IDynamicStackHandler cosmeticStackHandler = prevStacksHandler.getCosmeticStacks();
+
+                        for (int removedSlotIndex = 0; removedSlotIndex < stackHandler.getSlots(); removedSlotIndex++) {
+                            ItemStack stack = stackHandler.getStackInSlot(removedSlotIndex);
+                            if (!stack.isEmpty()) {
+                                instance.loseInvalidStack(stack);
+                            }
+
+                            ItemStack cosmeticStack = cosmeticStackHandler.getStackInSlot(removedSlotIndex);
+                            if (!cosmeticStack.isEmpty()) {
+                                instance.loseInvalidStack(cosmeticStack);
+                            }
+                        }
+                    }
+                }
+                sortedCurios.forEach((slotType, stacksHandler) -> curios.put(slotType.getIdentifier(), stacksHandler));
+                instance.setCurios(curios);
+            }
         }
     }
 }
--- src/main/java/curiousarmorstands/CuriousArmorStands.java
+++ src/main/java/curiousarmorstands/CuriousArmorStands.java
@@ -5,7 +5,6 @@ import net.minecraft.client.renderer.entity.ArmorStandRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ArmorStandEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
@@ -15,7 +14,6 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
@@ -27,11 +25,13 @@ import top.theillusivec4.curios.api.CuriosCapability;
 import top.theillusivec4.curios.api.SlotTypeMessage;
 import top.theillusivec4.curios.api.type.inventory.IDynamicStackHandler;
 
-@Mod("curious_armor_stands")
+@Mod(CuriousArmorStands.MODID)
 @SuppressWarnings("unused")
 public class CuriousArmorStands {
 
     public static final String MODID = "curious_armor_stands";
+
+    public static final String SLOT = "armor_stand_curio";
 
     @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModEvents {
@@ -50,7 +50,7 @@ public class CuriousArmorStands {
 
         @SubscribeEvent
         public static void enqueueIMC(final InterModEnqueueEvent event) {
-            InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("armor_stand_curio").cosmetic().size(8).build());
+            InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder(SLOT).cosmetic().size(8).hide().build());
         }
     }
 
@@ -65,13 +65,6 @@ public class CuriousArmorStands {
         }
 
         @SubscribeEvent
-        public static void entityJoinWorld(EntityJoinWorldEvent event) {
-            if (!event.getWorld().isRemote && event.getEntity() instanceof LivingEntity && !(event.getEntity() instanceof ArmorStandEntity)) {
-                CuriosApi.getSlotHelper().lockSlotType("armor_stand_curio", (LivingEntity) event.getEntity());
-            }
-        }
-
-        @SubscribeEvent
         public static void onEntityInteract(PlayerInteractEvent.EntityInteractSpecific event) {
             if (!(event.getTarget() instanceof ArmorStandEntity)) {
                 return;
@@ -81,7 +74,7 @@ public class CuriousArmorStands {
 
             if (!stackInHand.isEmpty()) {
                 equipItem(armorStand, stackInHand, event);
-            } else if (canUnEquipCurio(event.getLocalPos(), armorStand)){
+            } else if (canUnequipCurio(event.getLocalPos(), armorStand)){
                 unequipItem(armorStand, event);
             }
         }
@@ -89,11 +82,12 @@ public class CuriousArmorStands {
         public static void equipItem(ArmorStandEntity armorStand, ItemStack stackInHand, PlayerInteractEvent.EntityInteractSpecific event) {
             CuriosApi.getCuriosHelper().getCurio(stackInHand).ifPresent(curio -> CuriosApi.getCuriosHelper().getCuriosHandler(armorStand).ifPresent(handler -> {
                 if (!armorStand.world.isRemote) {
-                    handler.getStacksHandler("armor_stand_curio").ifPresent(stacksHandler -> {
+                    handler.getStacksHandler(SLOT).ifPresent(stacksHandler -> {
                         IDynamicStackHandler cosmeticStacks = stacksHandler.getCosmeticStacks();
                         for (int slot = 0; slot < cosmeticStacks.getSlots(); slot++) {
-                            if (cosmeticStacks.getStackInSlot(slot).isEmpty() && curio.canEquip("armor_stand_curio", armorStand)) {
+                            if (cosmeticStacks.getStackInSlot(slot).isEmpty() && curio.canEquip(SLOT, armorStand)) {
                                 cosmeticStacks.setStackInSlot(slot, stackInHand.copy());
+                                // noinspection deprecation
                                 curio.playRightClickEquipSound(armorStand);
                                 enableArmorStandArms(armorStand, stackInHand.getItem());
                                 if (!event.getPlayer().isCreative()) {
@@ -114,7 +108,7 @@ public class CuriousArmorStands {
         }
 
         public static void unequipItem(ArmorStandEntity armorStand, PlayerInteractEvent.EntityInteractSpecific event) {
-            CuriosApi.getCuriosHelper().getCuriosHandler(armorStand).ifPresent(handler -> handler.getStacksHandler("armor_stand_curio").ifPresent(stacksHandler -> {
+            CuriosApi.getCuriosHelper().getCuriosHandler(armorStand).ifPresent(handler -> handler.getStacksHandler(SLOT).ifPresent(stacksHandler -> {
                 IDynamicStackHandler cosmeticStacks = stacksHandler.getCosmeticStacks();
                 for (int slot = cosmeticStacks.getSlots() - 1; slot >= 0; slot--) {
                     ItemStack stackInSlot = cosmeticStacks.getStackInSlot(slot);
@@ -139,7 +133,7 @@ public class CuriousArmorStands {
             }
         }
 
-        private static boolean canUnEquipCurio(Vector3d localPos, ArmorStandEntity entity) {
+        private static boolean canUnequipCurio(Vector3d localPos, ArmorStandEntity entity) {
             boolean isSmall = entity.isSmall();
             double y = isSmall ? localPos.y * 2 : localPos.y;
             return !(entity.hasItemInSlot(EquipmentSlotType.FEET) && y >= 0.1 && y < 0.1 + (isSmall ? 0.8 : 0.45))
